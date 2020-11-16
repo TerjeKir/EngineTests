f2c75752baf64c03251425a277d28860d14ee328
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-11-16 22:47:18 +0100

    use visitor

--- sample_pgn.py
+++ sample_pgn.py
@@ -8,20 +8,41 @@ output = "datasets/samples.epd"
 samples_per_game = 10
 
 
+class Visitor(chess.pgn.BaseVisitor):
+    def begin_game(self):
+        self.fens = []
+        self.gameresult = ""
+        self.relevant = True
+
+    def visit_header(self, name, value):
+        if name == "Termination" and value != "adjudication":
+            self.relevant = False
+        if name == "Result":
+            self.gameresult = value
+
+    def end_headers(self):
+        if not self.relevant:
+            # Optimization hint: Do not even bother parsing the moves.
+            return chess.pgn.SKIP
+
+    # Will also be called for the terminal position of the game.
+    # Keep using visit_move instead, if that's not desired.
+    def visit_board(self, board):
+        if self.relevant:
+            self.fens.append(board.fen())
+
+    def result(self):
+        return self.gameresult, self.fens
+
+
 def sample_pgn():
     with open(dataset, 'r') as pgn, open(output, 'w') as out:
         while True:
-            game = chess.pgn.read_game(pgn)
-            if not game:
+            result, fens = chess.pgn.read_game(pgn, Visitor=Visitor)
+            if not fens:
                 break
-            result = game.headers["Result"]
-            game_length = int(game.headers["PlyCount"])
-            board = game.board()
-            samples = sample(range(game_length), min(samples_per_game, game_length))
-            for ply, move in enumerate(game.mainline_moves()):
-                board.push(move)
-                if ply in samples and not board.is_checkmate():
-                    print(board.fen() + " [%s]" % result, file=out)
+            for fen in sample(fens, min(samples_per_game, len(fens))):
+                print(fen + " [%s]" % result, file=out)
 
 
 if __name__ == "__main__":
