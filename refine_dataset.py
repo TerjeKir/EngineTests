0663ced3c95346ef381ffc3c8bea51eb7b75af09
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-11-17 05:22:35 +0100

    cleanup refine

--- refine_dataset.py
+++ refine_dataset.py
@@ -3,22 +3,24 @@ import chess
 
 
 dataset = "datasets/samples.epd"
+output = "datasets/refined.epd"
 
 engine_path = "engines/"
+engine_name = "weiss.exe"
 
 filter_limit = 1000
 
 
-def refine(engine_name):
+def refine():
 
     engine = Engine(engine_name, engine_path)
 
-    with open(dataset, 'r') as fens, open("datasets/refined.epd", 'w') as output:
+    with open(dataset, 'r') as fens, open(output, 'w') as out:
         for fen in fens:
             ## Do an x depth search and save the final given pv
             fen = ' '.join(fen.split()[:4]) + " 0 1"
             engine.position(fen)
-            engine.go(depth=8)
+            engine.go(depth=6)
 
             while True:
                 response = engine._readline()
@@ -41,8 +43,8 @@ def refine(engine_name):
             for move in pv:
                 board.push_uci(move)
 
-            print(board.fen(), file=output)
+            print(board.fen(), file=out)
 
 
 if __name__ == "__main__":
-    refine("weiss.exe")
+    refine()
