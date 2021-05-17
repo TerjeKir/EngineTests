25c3fa41ac783626dddd485a2414b3fba886674c
TerjeKir <42723273+TerjeKir@users.noreply.github.com>
2021-05-17 13:37:00 +0200

    Java support (#4)
    Should now work with .jar file engines.

--- uci_engine.py
+++ uci_engine.py
@@ -12,54 +12,50 @@ prefix = "" if win else "./"
 class Engine:
 
     def __init__(self, path, cwd=None):
-        self._process = Popen(prefix + path, shell=True, cwd=cwd, stdin=PIPE, stdout=PIPE, universal_newlines=True, bufsize=1)
+        _prefix = "java -jar " if "jar" in path else prefix
+        self._process = Popen(_prefix + path, shell=True, cwd=cwd, stdin=PIPE, stdout=PIPE, universal_newlines=True, bufsize=1)
         self.uci()
         self.set_option("Threads", 1)
         self.set_option("Hash", 32)
         self.isready()
 
-    def _msg_engine(self, msg):
+    def writeline(self, msg):
         self._process.stdin.write(msg)
 
-    def _readline(self):
-        return self._process.stdout.readline()
+    def readline(self):
+        return self._process.stdout.readline().rstrip()
 
 
     ### UCI commands
 
     def uci(self):
-        self._msg_engine("uci\n")
-        while True:
-            if self._readline() == "uciok\n":
-                break
+        self.writeline("uci\n")
+        while self.readline() != "uciok": pass
 
     def ucinewgame(self):
-        self._msg_engine("ucinewgame\n")
+        self.writeline("ucinewgame\n")
 
     def isready(self):
-        self._msg_engine("isready\n")
-        while True:
-            if self._readline() == "readyok\n":
-                break
+        self.writeline("isready\n")
+        while self.readline() != "readyok": pass
 
     def stop(self):
-        self._msg_engine("stop\n")
+        self.writeline("stop\n")
 
     def quit(self):
-        self._msg_engine("quit\n")
+        self.writeline("quit\n")
 
     def set_option(self, name, value):
-        self._msg_engine(f"setoption name {name} value {value}\n")
+        self.writeline(f"setoption name {name} value {value}\n")
 
     def position(self, fen=startpos, moves=None):
         moves = f" moves {moves}" if moves else ""
-        self._msg_engine(f"position fen {fen}{moves}\n")
+        self.writeline(f"position fen {fen}{moves}\n")
 
     # limitstring can be used to provide limits not supported by other arguments
-    def go(self, mate=None, movetime=None, depth=None, limitstring=""):
-        limits = ' '.join(f"{key} {value}" for key, value in locals().items() if key not in ("self", "limitstring") and value)
-        limits += f" {limitstring}" if limitstring else ""
-        self._msg_engine(f"go {limits}\n")
+    def go(self, **limits):
+        limits = ' '.join(f"{key} {value}" for key, value in limits.items())
+        self.writeline(f"go {limits}\n")
 
 
     ### Non-UCI commands
@@ -68,8 +64,8 @@ class Engine:
     # The engine should respond with an evaluation from white's point of view
     def eval(self, fen):
         self.position(fen)
-        self._msg_engine("eval\n")
-        return int(self._readline())
+        self.writeline("eval\n")
+        return int(self.readline())
 
     ## 'perft <depth> <fen>' or just 'perft <depth>' after a position command
     # The engine should print the perft count as a line with a single integer or
@@ -77,11 +73,11 @@ class Engine:
     def perft(self, fen, depth):
         self.isready()
         self.position(fen)
-        self._msg_engine(f"perft {depth} {fen}\n")
+        self.writeline(f"perft {depth} {fen}\n")
         while True:
-            response = self._readline().lower()
+            response = self.readline().lower()
             # A line with just a single number should be the perft count
-            if response.strip().isdigit():
+            if response.isdigit():
                 return int(response)
             # A line with 'nodes' should have the perft count as the first subsequent number
             if 'nodes' in response:
